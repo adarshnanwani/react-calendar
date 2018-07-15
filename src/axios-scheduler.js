0ae28dbaab783442928d3b9f4febdf41a56048b5
Adarsh Nanwani <adarsh@pratian.com>
2018-07-15 14:26:46 +0530

    Added axios with --save. Data now getting posted to firebase

--- src/axios-scheduler.js
+++ src/axios-scheduler.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL:''
+    baseURL: 'https://react-scheduler-5a700.firebaseio.com/'
 });
 
 export default instance;
--- src/components/NewAppointment/Form/Form.js
+++ src/components/NewAppointment/Form/Form.js
@@ -8,10 +8,13 @@ const form = (props) => {
         <div className={classes.form}>
             <form>
                 <label>Appointment Title:</label>
-                <Input inputType="input" type="text" changed={(event)=>props.titleChanged(event)} value={props.title} />
+                <Input inputType="input" type="text" changed={(event) => props.titleChanged(event)} value={props.title} />
                 <label>Select date and time:</label>
-                <Calendar onChange={(event)=> props.dateTimeChanged(event)} className={classes.dateTime}/>
-                <label className={classes.selectionLabel}>Current Selection:</label><span >{props.selectedDateTime?props.selectedDateTime:"Please select a date"}</span>
+                <Calendar onChange={(event) => props.dateTimeChanged(event)} className={classes.dateTime} />
+                <section className={classes.currentSelection}>
+                    <label className={classes.selectionLabel}>Current Selection:</label>
+                    <span>{props.selectedDateTime ? props.selectedDateTime : "Please select a date"}</span>
+                </section>
                 <button onClick={props.onClickHandler}>Schedule Appointment</button>
             </form>
         </div>
